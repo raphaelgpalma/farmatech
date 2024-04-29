be2b7ef48a6cfa6f162eba38cbf4797b02c2a2b1
raphaelgpalma <raphaelgpalma@icloud.com>
2024-04-29 14:27:23 -0300

    Set java project for backend

---
+++ core/core/src/main/java/com/farmatech/core/FarmatechApplication.java
@@ -0,0 +1,13 @@
+package com.farmatech.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmatechApplication.class, args);
+	}
+
+}
---
+++ core/core/src/test/java/com/farmatech/core/FarmatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmatech.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
